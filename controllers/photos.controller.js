467eaf96671cf59ed8b12fe1f1ae1d44fa0de159
Szymon <sszulinski@gmail.com>
2021-07-17 10:16:14 +0200

    Add checking if the file extension is gif, jpg or png && checking if the title length is <=25 && title author <=50

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,46 +1,41 @@
 const Photo = require('../models/photo.model');
 
 /****** SUBMIT PHOTO ********/
-
 exports.add = async (req, res) => {
-
   try {
     const { title, author, email } = req.fields;
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    if(title && author && email && file) { // if all fields are not empty
+      if (title.length > 25 || author.length > 50) throw new Error('Too many characters!')
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
-
+      const fileExt = fileName.split('.').slice(-1)[0];
+      
+      if(fileExt === 'gif' || fileExtt === 'jpg' || fileExt === 'png') {
+        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+        await newPhoto.save(); 
+        res.json(newPhoto);
+      } else throw new Error('Wrong file type!');
     } else {
       throw new Error('Wrong input!');
     }
-
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
-
 exports.loadAll = async (req, res) => {
-
   try {
     res.json(await Photo.find());
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** VOTE FOR PHOTO ********/
-
 exports.vote = async (req, res) => {
-
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
@@ -52,5 +47,4 @@ exports.vote = async (req, res) => {
   } catch(err) {
     res.status(500).json(err);
   }
-
 };
